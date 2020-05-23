0e4485fedf8504e77632599be2ecb350bf150d70
hanwenchen <2099368604@qq.com>
2020-05-23 20:35:42 +0800

    add

---
+++ pert_single.py
@@ -0,0 +1,60 @@
+import numpy as np
+import torch
+
+def pert_single(image, f, num_classes=10, overshoot=0.02, max_iter=10):
+
+    """
+       :param image: Image of size HxWx3
+       :param f: feedforward network (input: images, output: values of activation BEFORE softmax).
+       :param num_classes: num_classes (limits the number of classes to test against, by default = 10)
+       :param overshoot: used as a termination criterion to prevent vanishing updates (default = 0.02).
+       :param max_iter: maximum number of iterations for deepfool (default = 10)
+       :return: minimal perturbation that fools the classifier, number of iterations that it required, new estimated_label and perturbed image
+    """
+    loop_i = 0
+    
+    input_shape = image.shape
+    w = torch.zeros(input_shape)
+    r_tot = torch.zeros(input_shape)
+    
+    image.requires_grad_() = True
+    pert_image = image.clone()
+    label = None
+    k_i = None
+    
+    while loop_i < max_iter:
+        out = f(pert_image).view(-1)
+        val,arg = out.sort(descending = True)
+        val = val[:num_classes] - val[0]
+        arg = arg[:num_classes]
+        k_i = int(arg[0])
+        
+        if loop_i == 0:
+            label = int(arg[0])
+        
+        if k_i != label:
+            break
+        
+        gradients = []
+        for k in range(1,num_classes):
+            pert_image.grad.zero_()
+            f.zero_grad()
+            val[k].backward()
+            gradients.append(pert_image.grad)
+        
+        pert = np.inf
+        for k in range(1, num_classes):
+            pert_k = abs(val[k])/np.linalg.norm(gradients[k])
+
+            # determine which k to use in gradients
+            if pert_k < pert:
+                pert = pert_k
+                w = gradients[k]
+        
+        r_i =  pert * w / np.linalg.norm(w)
+        r_tot = (r_tot + r_i)*(1+overshoot)
+        
+        pert_image = image + r_tot
+        loop_i += 1 
+
+    return r_tot, loop_i, k_i, pert_image
---
+++ pert_universal.py
@@ -0,0 +1,74 @@
+import numpy as np
+import torch
+from pert_single import pert_single
+
+def proj_lp(v, xi, p):
+
+    # Project on the lp ball centered at 0 and of radius xi
+
+    # SUPPORTS only p = 2 and p = Inf for now
+    if p == 2:
+        v = v * min(1, xi/np.linalg.norm(v.view(-1)))
+        # v = v / np.linalg.norm(v.flatten(1)) * xi
+    elif p == np.inf:
+        v = np.sign(v) * np.minimum(abs(v), xi)
+    else:
+         raise ValueError('Values of p different from 2 and Inf are currently not supported...')
+
+    return v
+
+def pert_universal(trainloader, device, f, delta=0.2, epochs = 100, xi=10, p=np.inf, num_classes=10, overshoot=0.02, max_iter_df=10):
+    """
+    what is fooling ?
+    1. prediction(with perturbation) != ground truth
+    2. prediction(with perturbation) != original prediction
+    use 2. here, 1. should be easier
+    
+    :param delta: controls the desired fooling rate (default = 80% fooling rate)
+    :param epochs: optional other termination criterion (maximum number of iteration, default = 100)
+    :param xi: controls the l_p magnitude of the perturbation (default = 10)
+    :param p: norm to be used (FOR NOW, ONLY p = 2, and p = np.inf ARE ACCEPTED!) (default = np.inf)
+    :param num_classes: num_classes (limits the number of classes to test against, by default = 10)
+    :param overshoot: used as a termination criterion to prevent vanishing updates (default = 0.02).
+    :param max_iter_df: maximum number of iterations for deepfool (default = 10)
+    :return: the universal perturbation.
+    """
+    v = 0
+    fooling_rate = 0.0
+    for ep in range(epochs):
+        for batch_idx, (inputs, targets) in enumerate(trainloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            for i in range(inputs.shape[0]):
+                
+                with torch.no_grad():
+                    orig_pred = f(inputs[i:i+1]).view(-1).argmax()
+                    pert_pred = f(inputs[i:i+1]+v).view(-1).argmax()
+                
+                # fooling vec v is not sufficient
+                if orig_pred == pert_pred:
+                    dr,iters,_,_ = pert_single(inputs[i] + v, f, num_classes=num_classes, overshoot=overshoot, max_iter=max_iter_df)
+                    
+                    # Make sure it converged...
+                    if iters < max_iter_df-1:
+                        v = v + dr
+
+                        # Project on l_p ball
+                        v = proj_lp(v, xi, p)
+            
+        # fooling rate
+        fooled = 0
+        total = 0
+        with torch.no_grad():
+            for batch_idx, (inputs, targets) in enumerate(trainloader):
+                inputs, targets = inputs.to(device), targets.to(device)
+                orig_preds = f(inputs).argmax(1).numpy()
+                pert_preds = f(inputs+v).argmax(1).numpy()
+                fooled += np.sum(orig_preds != pert_preds) 
+                total += orig_preds.shape[0]
+            
+            fooling_rate = float(fooled)/total
+            print("epoch %d, batch %d, FOOLING RATE = %f" %(ep,batch_idx,fooling_rate))
+        
+        if fooling_rate >= 1-delta:
+            break
+    return v
